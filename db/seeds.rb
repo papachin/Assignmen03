b23abe64d2025204402aa96167aa39650a5b4502
MatthewR <matthewrice13@gmail.com>
2017-04-30 17:53:30 +0000

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
-GymClass.create!(classname:"Swimming", day:"Monday", time:"14:00", instructor:"Kelly.T", startdate:"27/04/2017")
-GymClass.create!(classname:"Yoga", day:"Monday", time:"15:30", instructor:"Laura.K", startdate:"27/04/2017")
-GymClass.create!(classname:"Spin", day:"Monday", time:"12:00", instructor:"Ryan.H", startdate:"27/04/2017")
+User.create!(name:  "userRoot",
+             email: "userRoot@email.com",
+             password:              "userRoot",
+             password_confirmation: "userRoot",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+             
+GymClass.create!(classname:"Running", day:"Tuesday", time:"14:00", instructor:"Kelly.T", startdate:"25/04/2017")
+GymClass.create!(classname:"Yoga", day:"Wednesday", time:"12:30", instructor:"Laura.K", startdate:"26/04/2017")
+GymClass.create!(classname:"Swimming", day:"Wednesday", time:"15:30", instructor:"Kelly.J", startdate:"26/04/2017")
+GymClass.create!(classname:"Yoga", day:"Thursday", time:"13:00", instructor:"Tony.P", startdate:"27/04/2017")
+GymClass.create!(classname:"Spin", day:"Friday", time:"13:30", instructor:"Laura.K", startdate:"28/04/2017")
