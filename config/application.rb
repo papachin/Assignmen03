893094f58a1c337e6a1f222cc7c703935a36e12f
MatthewR <matthewrice13@gmail.com>
2017-04-30 18:14:05 +0000

    all

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,6 @@ module SampleApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     
-    config.serve_static_files = true
+    #config.serve_static_files = true
   end
 end
